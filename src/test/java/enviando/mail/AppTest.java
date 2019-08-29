049e676f35b1ec7e5b6f1587abef81bdeefde0d5
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-08-29 20:31:16 -0300

    Criando autenticação com o gmail via Session

--- src/test/java/enviando/mail/AppTest.java
+++ src/test/java/enviando/mail/AppTest.java
@@ -2,6 +2,10 @@ package enviando.mail;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -14,8 +18,14 @@ import junit.framework.TestSuite;
 public class AppTest 
     
 {
+	private String userName = "jeffersonjavaweb@gmail.com";
+	private String senha = "987832042";
+	
+	
 	@org.junit.Test
  public void testeEmail() {
+		try {
+			
 		
 		Properties properties = new Properties();
 		properties.put("mail.smtp.auth", "true");/*Autorização*/
@@ -24,7 +34,19 @@ public class AppTest
 		properties.put("mail.smtp.port", "465");/*Porta do servidor*/
 		properties.put("mail.smtp.socketFactory.port", "465");/*Expecifica a porta a ser conectada pelo socket*/
 		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao smtp*/
-	 
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		
+		
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
  }
   
 }
